d4e1389fcbe705d0ce63d3ece022e9ff84e749ec
Dan Day <danday@gmail.com>
2016-05-27 15:31:15 -0500

    first commit

---
+++ java.js
@@ -0,0 +1,18 @@
+
+var x = 0;
+
+for (i=0; i<=10; i++) {
+  x += i*i;
+}
+
+console.log("sum of squares", x);
+
+var y = 0;
+
+for (i=0; i<=10; i++) {
+  y += i
+}
+
+y = y*y;
+
+console.log("square of sum", y);
